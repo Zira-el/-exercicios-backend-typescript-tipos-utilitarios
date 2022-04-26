55c628afa2bba969ff644ea6ec2dd0d4b9f4e8c7
Nilton Menezes <nilton.menezes@gafit.com.br>
2022-04-25 21:52:12 -0300

    exercicio 3

--- 03/scripts.ts
+++ 03/scripts.ts
@@ -6,7 +6,12 @@ type Conn = {
     dbname: string
 }
 
-const conexao = (dados: Conn) => {
+type Connection = {
+    driver: string,
+    url: string
+}
+
+const conexao = (dados: Conn): Readonly<Connection> => {
     const { username, password, host, port, dbname } = dados;
     return {
         driver: 'postgres',
